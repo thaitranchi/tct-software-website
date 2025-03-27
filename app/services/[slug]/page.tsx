97440cdd2815a7e896fbf2407fb33e0882f2a52f
thaitranchi <chithai1999@gmail.com>
2025-03-27 13:52:23 +0700

    resolve deployment conflict

--- app/services/[slug]/page.tsx
+++ app/services/[slug]/page.tsx
@@ -1,8 +1,8 @@
-import { useRouter } from "next/router";
+import { useParams } from "next/navigation";
 
 export default function ServicePage() {
-  const router = useRouter();
-  const { slug } = router.query;
+  const params = useParams();
+  const slug = params?.slug ? (Array.isArray(params.slug) ? params.slug[0] : params.slug) : "default-slug";
 
   return <h1>Service: {slug}</h1>;
 }
